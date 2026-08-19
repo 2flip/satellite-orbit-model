9a10ea61c6dfa23d40d656386cc4c81038f73e9a
Dmitry <165209126+2flip@users.noreply.github.com>
2026-08-19 12:13:53 +0400

    Add main file

---
+++ main.py
@@ -0,0 +1,20 @@
+
+import numpy as np 
+
+
+
+#constant variables
+angle = 45      #degrees
+mass = 1        #kilogram
+velocity = 100  #m\s
+gravity = 9.81  #m\s^2
+cd = 0.8        #coefficient 
+
+
+#step
+n_step = 1000
+
+#boundary_condition
+print('1')
+
+
