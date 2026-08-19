3361d2f81f496d3da3e42305ac8882af981d437e
2flip <turbo@URBK5000.localdomain>
2026-08-19 13:11:12 +0400

    Add basic parametrs and start write code)

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
-
-import numpy as np 
-
-
-
-#constant variables
-angle = 45      #degrees
-mass = 1        #kilogram
-velocity = 100  #m\s
-gravity = 9.81  #m\s^2
-cd = 0.8        #coefficient 
+import numpy
+import matplotlib as plt
 
 
-#step
-n_step = 1000
+#<----------------------START USER'S PARAMETRS---------------------->
 
-#boundary_condition
-print('1')
+#Parametrs_satellite
+mass_sat = 500                          #kg
+coeff_drag_sat = 2.2 
+area_sat = 2                            #m^2
+
+#Parametrs_use_satellite
+time_use = 5.5                          #years
+
+#Parametrs_orbite\enviroment
+sun_active = True                          #not use yet
+orbite_radius = 550                     #km, only circle orbite
+
+#<----------------------END USER'S PARAMETRS---------------------->
+
+#CONSTANT
+const radius_earth = 6.371e6            #m
+const gravity_cons_earth = 3.986e14     #m^3/s^2
+
+
+
+
+
+velocity_sat = 
+
+#Calc_needless_parametrs
+force_drag = 0.5*coeff_drag_sat*area_sat*velocity_sat
 
 
